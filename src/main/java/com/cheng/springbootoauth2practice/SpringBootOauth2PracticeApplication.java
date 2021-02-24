1dd210b0c42cacdc9bd86c528fcaa736670cf5d0
chengtong <chengt@xports.cn>
2021-02-24 20:22:50 +0800

    Build project

---
+++ src/main/java/com/cheng/springbootoauth2practice/SpringBootOauth2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cheng.springbootoauth2practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheng/springbootoauth2practice/SpringBootOauth2PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheng.springbootoauth2practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
